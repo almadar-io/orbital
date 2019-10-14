da3eeda9f208e38d056b6477ce23293953f19cf6
itechdom <oalghnmi@gmail.com>
2019-10-14 07:46:50 -0400

    orbital

--- react-views-service.js
+++
@@ -1,6 +0,0 @@
-import rootStore from "./Store/Store";
-import * as Material from "./Templates/Material";
-import * as MaterialPro from "./Templates/Material-Pro";
-import * as Models from "./Models";
-
-export { rootStore, Material, MaterialPro, Models };
