7ab5c17ccfa341d02d7f46244de6feaed1526223
itechdom <oalghnmi@gmail.com>
2019-10-18 06:03:08 -0400

    new build

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import withOrbital from "./withOrbital";
 import offlineStorage from "./OfflineStorage/OfflineStorage";
 import User from "./User/User";
 import Settings from "./Settings/Settings";
-export { withOrbital, offlineStorage, User, Settings };
+import Admin from "./Admin/Admin";
+export { withOrbital, offlineStorage, User, Settings, Admin };
