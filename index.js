a727257ad348e9a24853dfeacfc7c5ec85e89c16
itechdom <oalghnmi@gmail.com>
2019-10-30 20:59:35 -0400

    with orbital mobile

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import withOrbital from "./withOrbital";
+import withOrbitalMobile from "./withOrbitalMobile";
 import offlineStorage from "./OfflineStorage/OfflineStorage";
 import User from "./User/User";
 import Settings from "./Settings/Settings";
 import Admin from "./Admin/Admin";
-export { withOrbital, offlineStorage, User, Settings, Admin };
+export {
+  withOrbital,
+  withOrbitalMobile,
+  offlineStorage,
+  User,
+  Settings,
+  Admin
+};
